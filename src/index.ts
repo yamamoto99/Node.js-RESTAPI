16fb89fe42e9a44c9e577a50814cfe7ba5c6f2bc
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-05-29 21:45:22 +0900

    feat:add-comment

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,23 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 
+// Expressアプリケーションの作成
 const app = express();
 
+// CORSの設定
 app.use(cors({
 	credentials: true,
 }))
 
+// ミドルウェアの設定
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
+// HTTPサーバーの作成
 const server = http.createServer(app);
 
+// サーバーの起動
 server.listen(8000, () => {
 	console.log('Server is running on http://localhost:8000/');
 });
