d4e2aeb91ea1d28f17c50c5f8070a3a40de39fb7
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-05-29 22:13:50 +0900

    feat:mongoDB-setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 // Expressアプリケーションの作成
 const app = express();
@@ -25,3 +26,13 @@ const server = http.createServer(app);
 server.listen(8000, () => {
 	console.log('Server is running on http://localhost:8000/');
 });
+
+// MongoDBの設定
+const MONGO_URL = "mongodb+srv://yamamoto99:Dd0Nl2Gl62mCrmap@cluster0.as5rwdb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Promiseの設定
+mongoose.Promise = Promise;
+// 設定されたURLでMongoDBに接続
+mongoose.connect(MONGO_URL);
+// 'error'イベントが発生した場合Error型のerrorを受け取り、コンソールに出力
+mongoose.connection.on('error', (error: Error) => console.log(error));
