c7d14b6a85aca1be6469dca09e01ace5b836ba80
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-05-30 23:15:56 +0900

    feat:login

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,6 +3,49 @@ import express from "express";
 import {getUserByEmail, createUser} from "../db/users";
 import {authentication, random} from "../helper";
 
+export const login = async (req: express.Request, res: express.Response) => {
+	try {
+		const {email, password} = req.body;
+
+		// メールアドレスとパスワードが存在しない場合は400を返す
+		if (!email || !password) {
+			return res.sendStatus(400);
+		}
+
+		// メールアドレスを使用してユーザーを検索
+		const user = await getUserByEmail(email).select("+authentication.salt +authentication.password +username +email");
+		if (!user) {
+			// ユーザーが存在しない場合は400を返す
+			return res.sendStatus(400);
+		}
+
+		// パスワードとsaltを使用してHMACを生成するauthentication関数を使用
+		const expectedHash = authentication(user.authentication.salt, password);
+		if (user.authentication.password !== expectedHash) {
+			// パスワードが一致しない場合は403を返す
+			return res.sendStatus(403);
+		}
+
+		// saltを生成
+		const salt = random();
+		// saltとユーザーIDを使用してセッショントークンを生成
+		user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+		// userドキュメント更新
+		await user.save();
+
+		// クッキーにセッショントークンを設定
+		res.cookie("REST_AUTHENTICATION", user.authentication.sessionToken, { domain: "localhost", path: "/"});
+
+		// 200OKとユーザー情報を返す
+		return res.status(200).json(user).end();
+	} catch (error) {
+		// エラーが発生した場合は400を返す
+		console.error(error);
+		return res.sendStatus(400);
+	}
+};
+
 export const register = async (req: express.Request, res: express.Response) => {
 	try{
 		// リクエストボディからユーザー名、メールアドレス、パスワードを取得
--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 
 import { register } from '../controllers/authentication';
+import { login } from '../controllers/authentication';
 
 export default (router: express.Router) => {
 	// /auth/registerにPOSTリクエストがあった場合register関数を実行
 	router.post('/auth/register', register);
+	router.post('/auth/login', login);
 };
