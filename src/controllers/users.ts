79226199905c59e2670b3ca404c8e47344548b78
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-05-31 15:11:13 +0900

    feat:user-delete

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,10 @@
 import exprees from 'express';
 
-import { getUsers } from '../db/users';
+import { getUsers, deleteUserById } from '../db/users';
 
 export const getAllUsers = async (req: exprees.Request, res: exprees.Response) => {
 	try {
+		// 全ユーザー情報を取得
 		const users = await getUsers();
 		return res.status(200).json(users);
 	} catch (error) {
@@ -12,3 +13,18 @@ export const getAllUsers = async (req: exprees.Request, res: exprees.Response) =
 		res.sendStatus(400);
 	}
 };
+
+export const deleteUser = async (req: exprees.Request, res: exprees.Response) => {
+	try {
+		// リクエストパラメータからユーザーIDを取得
+		const { id } = req.params;
+		// IDからユーザーを削除
+		const deletedUser = await deleteUserById(id);
+		// 削除されたユーザー情報を返す
+		return res.json(deletedUser);
+	} catch (error) {
+		// エラーが発生した場合は400を返す
+		console.error(error);
+		res.sendStatus(400);
+	}
+}
