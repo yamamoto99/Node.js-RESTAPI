871b200a94f26936bb057fb9f3ff0a9b4ecabd49
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-05-31 15:48:07 +0900

    feat:update-username

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import exprees from 'express';
 
-import { getUsers, deleteUserById } from '../db/users';
+import { getUsers, deleteUserById, getUserById } from '../db/users';
 
 export const getAllUsers = async (req: exprees.Request, res: exprees.Response) => {
 	try {
@@ -28,3 +28,27 @@ export const deleteUser = async (req: exprees.Request, res: exprees.Response) =>
 		res.sendStatus(400);
 	}
 }
+
+export const updateUser = async (req: exprees.Request, res: exprees.Response) => {
+	try {
+		// リクエストパラメータからユーザーIDを取得
+		const { id } = req.params;
+		// リクエストボディからユーザー名を取得
+		const { username } = req.body;
+		// ユーザー名が存在しない場合は400を返す
+		if (!username) {
+			return res.sendStatus(400);
+		}
+		// IDからユーザーを取得
+		const user = await getUserById(id);
+		// ユーザー名を更新
+		user.username = username;
+		await user.save();
+
+		return res.status(200).json(user).end();
+	} catch (error) {
+		// エラーが発生した場合は400を返す
+		console.error(error);
+		res.sendStatus(400);
+	}
+};
