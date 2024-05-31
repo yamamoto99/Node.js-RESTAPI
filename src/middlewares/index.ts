07f2071dbf3fb5c5aad908dfe41e80cbb965c688
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-05-31 11:21:32 +0900

    feat:check-login

---
+++ src/middlewares/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import {get, merge} from 'lodash';
+
+import { getUserBySessionToken } from 'db/users';
+
+export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+	try {
+		const sessionToken = req.cookies["REST_AUTHENTICATION"];
+		// セッショントークンが存在しない場合は403を返す
+		if (!sessionToken) {
+			return res.sendStatus(403);
+		}
+
+		const existingUser = await getUserBySessionToken(sessionToken);
+		// ユーザーが存在しない場合は403を返す
+		if (!existingUser) {
+			return res.sendStatus(403);
+		}
+
+		// リクエストオブジェクトにユーザー情報を追加
+		merge(req, {indentify: existingUser});
+
+		return next();
+	} catch (error) {
+		// エラーが発生した場合は400を返す
+		console.error(error);
+		return res.sendStatus(400);
+	}
+}
